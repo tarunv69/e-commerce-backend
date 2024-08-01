e8f10194dac4446f1796991ad4be8e426d2768cb
tarunv69 <tarun.venkatesh007@gmail.com>
2024-08-01 15:11:29 +0530

    fix: s3

--- src/uploads/s3.service.ts
+++ src/uploads/s3.service.ts
@@ -9,7 +9,7 @@ export class S3Service {
 
   constructor() {
     this.s3 = new S3Client({
-      region: process.env.AWS_REGION,
+      region: 'ap-south-1',
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
